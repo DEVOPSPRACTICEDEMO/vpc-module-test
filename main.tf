58364607691f3ea515791f46d1dc7431fbe8a197
Suraj Padhi <surajpadhi4546@gmail.com>
2025-06-27 13:16:06 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
-    project = "roboshop"
+     source = "../terraform-aws-vpc"
+    /*project = "roboshop"
     environment = "dev"
-    public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
-  
+    public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"] */
+    project =var.project  
+    environment = var.environment
+    private_subnet_cidr = var.private_subnet_cidr
+    public_subnet_cidr = var.public_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
+
+    is_peering_required = true
 }
